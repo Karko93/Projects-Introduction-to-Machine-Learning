7f8497da2bdab8c1626c7729147bb36e64d8500d
Karko93 <41451360+Karko93@users.noreply.github.com>
2020-03-10 08:51:29 +0100

    Code Cleanup and Converting

--- Task1a/Task1a_Ketzel.py
+++ Task1a/Task1a_Ketzel.py
@@ -8,56 +8,39 @@ import pandas as pd
 import numpy as np
 import sklearn as sk
 from sklearn.linear_model import Ridge
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 
 
-# In[2]:
-
-
-## Reading data from csv-file and compart into input and output
+## Reading data from csv-file and compartment into input and output
 train = pd.read_csv("train.csv")
 X = np.array(train.drop(columns = ["Id", "y"]))
 y = np.array(train.get("y"))
 
+##Data Preprocessing
 
-
-## data preprocessing
-
-#Ridge Regression
-cv = KFold(n_splits=10,shuffle=True)
+#Ridge Regression with K-Fold
+cv = KFold(n_splits=10)
 cv.get_n_splits(X)
-alphas = np.array([0.01])
+alphas = np.array([0.01, 0.1 , 1 , 10 , 100])
 RMSE_avg = []
+
 for alph in alphas:
     RMSE = []
-    
+    clf = Ridge(alpha= alph, normalize= True, fit_intercept=False)
     for train_index, test_index in cv.split(X):
-        #print('\n', test_index)
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        clf = Ridge(alpha= alph, normalize= True, fit_intercept=False)
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
-        RMSE.append(sk.metrics.mean_squared_error(y_test, y_pred))
+        RMSE.append(np.sqrt(sk.metrics.mean_squared_error(y_test, y_pred)))
     
     RMSE_avg.append(np.mean(RMSE))
-    
-   #print(np.mean(RMSE), '\n', alph, '\n', RMSE)
-    
-####################
-# Error evaluation #
-####################
+    print(np.mean(RMSE), '\n', alph, '\n', RMSE)
 
 
-#print(X_train.shape,X_test.shape, '\n', y_train.shape,y_test.shape)
-
-
-#print(sk.metrics.mean_squared_error(y_test, y_pred))
-#print(RMSE)
 result = pd.concat([pd.DataFrame(RMSE_avg)])
-#print(result)
+print(result)
 pd.DataFrame(result).to_csv("submit.csv", index=False, header=False, decimal='.', sep=' ', float_format='%.1f')
 
 
