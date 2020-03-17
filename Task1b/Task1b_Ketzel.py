bd0aed1d5beea981570b0161b9f3f1fc2ed733c1
Karko93 <41451360+Karko93@users.noreply.github.com>
2020-03-17 01:36:15 +0100

    Further Progress

--- Task1b/Task1b_Ketzel.py
+++ Task1b/Task1b_Ketzel.py
@@ -6,8 +6,9 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-
+import sklearn.linear_model as lm
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 # In[8]:
 
@@ -18,6 +19,9 @@ X = np.array(train.drop(columns = ["Id", "y"]))
 y = np.array(train.get("y"))
 print(X.shape)
 
+#X = preprocessing.normalize(X)
+
+
 
 # In[9]:
 
@@ -26,16 +30,19 @@ print(X.shape)
 x_extend = np.append(X, X**2, axis=1)
 x_extend = np.append(x_extend, np.exp(X), axis=1)
 x_extend = np.append(x_extend, np.cos(X), axis=1)
-x_extend = np.hstack((x_extend, np.ones((X.shape[0], 1), dtype=int)))
+x_extend = np.hstack((x_extend, np.ones((X.shape[0], 1), dtype= float)))
 print(x_extend.shape)
 
+x_train, x_test, y_train, y_test = train_test_split(x_extend, y, test_size=0.1, shuffle = True)
 
 # In[14]:
 
 
 ## Linear Regression and Getting Coefficients of Models
-reg = LinearRegression(fit_intercept= False, normalize=True)
-reg.fit(x_extend,y)
+#reg = lm.LinearRegression(fit_intercept= False, normalize= False)
+reg = lm.RidgeCV(fit_intercept=False, alphas = [0.1,1,10,100])
+reg.fit(x_train,y_train)
+y_hat = reg.predict(x_extend)
 Coeffs = reg.coef_
 print(Coeffs.shape)
 
@@ -45,12 +52,12 @@ print(Coeffs.shape)
 
 ## Save Coefficient in a csv
 result = pd.concat([pd.DataFrame(Coeffs)])
-np.set_printoptions(precision=0)
-pd.DataFrame(result).to_csv("submit.csv", index=False, header=False, decimal='.', sep=' ', float_format='%.0f')
-
+np.set_printoptions(precision=3)
+pd.DataFrame(result).to_csv("submit.csv", index=False, header=False, decimal='.', sep=' ' ,float_format='%.0f')
 Coeffs_Pre = np.array(pd.read_csv("submit_pre.csv", header = None))
 
 print('\n', Coeffs_Pre.T,'\n', Coeffs)
+print('\n','RMSE = ',np.sqrt(mean_squared_error(y,y_hat)))
 
 # In[ ]:
 
